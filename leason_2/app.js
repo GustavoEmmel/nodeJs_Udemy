6784e9208cac5158fef08fdcfe6d8f40853593de
Gustavo Reichelt Emmel <gremmel@gmail.com>
2017-11-12 11:20:14 -0200

    add new views

--- leason_2/app.js
+++ leason_2/app.js
@@ -3,15 +3,21 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+	res.render("home/index");
+});
+
+app.get('/form_add', function(req, res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+	res.render("noticias/noticias");
+});
+
 app.get('/tecno', function(req, res){
 	res.render("session/tecno");
 });
-
-
-app.get('/', function(req, res){
-	res.send("<html><body>ola mundo</body></html>");
-});
-
 
 app.listen(3000, function(){
 	console.log('servidor com express');
