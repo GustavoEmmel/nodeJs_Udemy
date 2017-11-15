f1145c864b5c54b028345431044fb6d6263f281f
Gustavo Reichelt Emmel <gremmel@gmail.com>
2017-11-15 11:17:52 -0200

    body-parser

--- leason_2/config/dbConnection.js
+++ leason_2/config/dbConnection.js
@@ -5,7 +5,7 @@ var connMySQL = function(){
 	return mysql.createConnection({
 		host : 'localhost', 
 		user : 'root', 
-		password : 'mysql',
+		password : '1234',
 		database : 'portal_noticias'
 	});	
 }
