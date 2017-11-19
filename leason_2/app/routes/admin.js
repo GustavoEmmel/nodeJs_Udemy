9da8a0c9deb4819c477db66cd957bb4223434089
Gustavo Reichelt Emmel <gremmel@gmail.com>
2017-11-19 11:41:37 -0200

    refactor to start using controllers - mvc pattern

--- leason_2/app/routes/admin.js
+++ leason_2/app/routes/admin.js
@@ -1,31 +1,9 @@
 module.exports = function(app){
 	app.get('/form_add', function(req, res){
-		res.render("admin/form_add_noticia");
+		app.app.controllers.admin.form_add(app, req, res);
 	});	
 
 	app.post('/noticias/salvar', function(req, res){
-		var noticia = req.body;
-
-		req.assert('titulo','Titulo é obrigatorio').notEmpty();
-		req.assert('resumo','Resumo é obrigatorio').notEmpty();
-		req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-		req.assert('autor','autor é obrigatorio').notEmpty();
-		req.assert('data_noticia','data é obrigatorio').notEmpty();
-		req.assert('noticia','noticia é obrigatorio').notEmpty();
-
-		var erros = req.validationErrors();
-
-		if (erros) {
-			res.render("admin/form_add_noticia");	
-			return;
-		}
-
-		var connection = app.config.dbConnection();
-		var noticiasModel = new app.app.models.NoticiasDAO(connection);
-
-		noticiasModel.salvarNoticia(noticia, function(error, result){
-			res.redirect("/noticias");
-		});
-
+		app.app.controllers.admin.noticias_salvar(app, req, res);
 	});	
 }
